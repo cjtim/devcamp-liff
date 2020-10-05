984a672c69626adbd96625f60031dfc1edf482bd
Tim <iam@cjtim.com>
2020-10-05 23:06:22 +0700

    fix button not redirect

--- src/page/redirect/index.js
+++ src/page/redirect/index.js
@@ -7,7 +7,7 @@ function Redirect() {
   const newUrl = 'scbeasysim://purchase/' + orderid
   document.location.href = newUrl
   return <Flex align="center" justify="center">
-    <Button bg="#4e2e7f" href={newUrl} color='white'>Pay with SCB</Button>
+    <Button bg="#4e2e7f" href={newUrl} color='white' onClick={() => document.location.href = newUrl}>Pay with SCB</Button>
   </Flex>
 }
 export default Redirect
