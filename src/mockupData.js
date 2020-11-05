70091a17c4b1eb28b931e6aed681acd3fe4bd826
Tim <iam@cjtim.com>
2020-11-05 19:08:07 +0700

    add scb payment by create order and transaction

--- src/mockupData.js
+++ src/mockupData.js
@@ -16,3 +16,34 @@ export const allRestaurant = [
     location: 'บาร์วิศวะ'
   }
 ]
+
+export const orderPayload = {
+  selectedMenu: [
+    {
+      menuId: '6565a71c-4734-4f10-b8e5-46b9d1819893',
+      note: 'note',
+      unit: 1
+    },
+    {
+      menuId: 'adbad266-c0e7-487a-b7e7-03519b3542e3',
+      note: 'note',
+      unit: 1
+    },
+    {
+      menuId: 'c2a3771f-12e2-452b-94b8-f8428ecf2843',
+      note: 'note',
+      unit: 1
+    },
+    {
+      menuId: 'e3f743a2-0425-466c-a0a2-829b851f8d3e',
+      note: 'note',
+      unit: 1
+    },
+    {
+      menuId: '7ba05733-31fd-48c4-be78-a26eff81a138',
+      note: 'note',
+      unit: 1
+    }
+  ],
+  restaurantId: 'd8a7a4cf-ecd9-4725-9f83-ab8fda676190'
+}
--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,53 +1,61 @@
 // import { lineState } from '../../recoil'
 // import { useRecoilState } from 'recoil'
 import React, { useState } from 'react'
-import { Input, Flex, Button, Link } from '@chakra-ui/core'
+import { Flex, Button } from '@chakra-ui/core'
 import liff from '@line/liff'
 import axios from 'axios'
 import LoadingAnimation from './../../component/loadingAnimation'
+import { orderPayload } from './../../mockupData'
 
 const backendInstance = axios.create({
-  baseURL: 'https://restaurant-helper-omise-nwbwsoebza-an.a.run.app'
+  baseURL: process.env.REACT_APP_BACKEND_URL
 })
 
 export function Home() {
-  const [amount, setAmount] = useState(100)
   const [isLoading, setisLoading] = useState(false)
-  if (isLoading) return <LoadingAnimation/>
+  if (isLoading) return <LoadingAnimation />
   return (
     <main>
       <Flex justify="center" py={4}>
-        <Input
-          width="80%"
-          al
-          placeholder="Amount"
-          size="lg"
-          defaultValue={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-      </Flex>
-      {/* Omise */}
-      <Flex justify="center" py={4}>
-        <Link href="/menu/1">
-          <Button variantColor="green">Pay with Omise</Button>
-        </Link>
-      </Flex>
-      {/* SCB */}
-      <Flex justify="center" py={4}>
-        <Button bg="#4e2e7f" color='white' onClick={async() => {
+        <Button
+          bg="#4e2e7f"
+          color="white"
+          onClick={async () => {
             setisLoading(true)
-            const payload = await backendInstance.post('/payment/scb', {
-              amount: amount
-            }, {
-              headers: {
-                authorization: `Bearer ${liff.getAccessToken()}`
-              }
+            const scb = await createSCBLink()
+            liff.openWindow({
+              url: '/redirect?url=' + scb.deeplinkUrl,
+              external: false
             })
-            liff.openWindow({ url:'/redirect?url=' + payload.data, external: false})
             setisLoading(false)
-          }}>Pay with SCB</Button>
+          }}
+        >
+          Pay with SCB
+        </Button>
       </Flex>
-
     </main>
   )
+}
+
+async function createSCBLink() {
+  const payload = await backendInstance.post('/order/create', orderPayload, {
+    headers: {
+      authorization: `Bearer ${liff.getAccessToken()}`
+    }
+  })
+  console.log(payload.data)
+  const scb = await backendInstance.post(
+    '/transaction/create',
+    {
+      payAmount: payload.data.totalAmount,
+      orderId: payload.data.id
+    },
+    {
+      headers: {
+        authorization: `Bearer ${liff.getAccessToken()}`
+      }
+    }
+  )
+  console.log(scb.data)
+  return scb.data
 }
