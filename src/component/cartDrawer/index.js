bd043f16f2bcd56a7aec87c514bcc059aa1bcde2
Tim <iam@cjtim.com>
2020-11-14 22:56:17 +0700

    cleanup cart

--- src/component/cartDrawer/index.js
+++ src/component/cartDrawer/index.js
@@ -18,119 +18,85 @@ import { cart as atomCart } from '../../recoil'
 import { useRecoilState } from 'recoil'
 import { MenuCard } from '../menuCard'
 
+const backendInstance = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_URL
+})
+
 export default function CartDrawer() {
-  const [cart, setCart] = useRecoilState(atomCart)
-  const [isLoading, setIsLoading] = React.useState(false)
+  // Drawer controller
   const { isOpen, onOpen, onClose } = useDisclosure()
   const btnRef = React.useRef()
+  // State
+  const [cart, setCart] = useRecoilState(atomCart)
+  const [isLoading, setIsLoading] = React.useState(false)
+  // First load
   React.useEffect(() => {
     ;(async () => {
       await liff.ready
       backendInstance.defaults.headers['authorization'] = `Bearer ${liff.getAccessToken()}`
       console.log((await backendInstance.get('')).data)
+      // Get cart data from localstorage if exist
+      // localstorage is not realtime sync like recoil
       const localCart = localStorage.getItem('cart')
       if (localCart) setCart(JSON.parse(localCart))
-
     })()
   }, [])
+
+  function onClearBtn() {
+    onClose()
+    setCart([])
+    localStorage.setItem('cart', [])
+  }
+
+  async function onCheckoutBtn() {
+    setIsLoading(true)
+    try {
+      const scb = await checkout(cart)
+      window.open('/redirect?url=' + scb.deeplinkUrl)
+      // window.location.pathname = '/redirect?url=' + scb.deeplinkUrl
+      // liff.openWindow({
+        // url: '/redirect?url=' + scb.deeplinkUrl
+      // })
+      setCart([])
+      localStorage.setItem('cart', [])
+      onClose()
+    } catch (e) {
+      alert(e.message)
+    }
+    setIsLoading(false)
+  }
+
   return (
     <>
-      <IconButton
-        aria-label="Search database"
-        ref={btnRef}
-        colorScheme="teal"
-        icon={<HamburgerIcon />}
-        onClick={onOpen}
-        position="fixed"
-        right="10px"
-        
-      >
-        Open
-      </IconButton>
-      <Drawer
+      <Cart
         isOpen={isOpen}
-        placement="right"
+        onOpen={onOpen}
         onClose={onClose}
-        finalFocusRef={btnRef}
-        size={'xl'}
+        btnRef={btnRef}
+        onClearBtn={onClearBtn}
+        onCheckoutBtn={async () => onCheckoutBtn()}
       >
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Cart</DrawerHeader>
-
-            <DrawerBody>
-              {isLoading && (
-                <Flex
-                  justify="center"
-                  alignItems="center"
-                  justifyContent="center"
-                  height="50%"
-                >
-                  <Spinner size="xl" thickness="4px" />
-                </Flex>
-              )}
-              {!isLoading &&
-                cart.selectedMenu &&
-                cart.selectedMenu.map((i, index) => {
-                  return (
-                    <MenuCard
-                      key={index}
-                      name={i.name}
-                      img={i.img}
-                      url={'/menu/' + i.id}
-                      price={i.unit}
-                    />
-                  )
-                })}
-            </DrawerBody>
-
-            <DrawerFooter>
-              <Button
-                variant="outline"
-                mr={3}
-                onClick={() => {
-                  onClose()
-                  setCart([])
-                  localStorage.setItem('cart', [])
-                }}
-              >
-                Clear
-              </Button>
-              <Button
-                colorScheme="blue"
-                textColor="white"
-                onClick={async () => {
-                  setIsLoading(true)
-                  try {
-                    const scb = await createSCBLink(cart)
-                    liff.openWindow({
-                      url: '/redirect?url=' + scb.deeplinkUrl
-                    })
-                    setCart([])
-                    localStorage.setItem('cart', [])
-                    onClose()
-                  } catch (e) {
-                    alert(e.message)
-                  }
-                  setIsLoading(false)
-                }}
-              >
-                Checkout
-              </Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
+        {isLoading && <LoadingSpinner />}
+        {!isLoading &&
+          cart.selectedMenu &&
+          cart.selectedMenu.map((i, index) => {
+            return (
+              <MenuCard
+                key={index}
+                name={i.name}
+                img={i.img}
+                url={'/menu/' + i.id}
+                price={i.unit}
+              />
+            )
+          })}
+          {!cart.selectedMenu && !isLoading && "Cart is empty"}
+      </Cart>
     </>
   )
 }
 
-const backendInstance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL
-})
-
-async function createSCBLink(cart) {
+async function checkout(cart) {
   const payload = await backendInstance.post('/order/create', cart)
   console.log(payload.data)
   const scb = await backendInstance.post('/transaction/create', {
@@ -139,4 +105,46 @@ async function createSCBLink(cart) {
   })
   console.log(scb.data)
   return scb.data
+}
+
+function LoadingSpinner() {
+  return (
+    <Flex justify="center" alignItems="center" justifyContent="center" height="50%">
+      <Spinner size="xl" thickness="4px" />
+    </Flex>
+  )
+}
+function Cart({ children, isOpen, onOpen, onClose, btnRef, onClearBtn, onCheckoutBtn }) {
+  return (
+    <>
+      <IconButton
+        aria-label="Cart Icon"
+        ref={btnRef}
+        colorScheme="teal"
+        icon={<HamburgerIcon />}
+        onClick={onOpen}
+        position="fixed"
+        right="10px"
+      >
+        Open
+      </IconButton>
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={btnRef} size="xl">
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Cart</DrawerHeader>
+            <DrawerBody>{children}</DrawerBody>
+            <DrawerFooter>
+              <Button variant="outline" mr={3} onClick={onClearBtn}>
+                Clear
+              </Button>
+              <Button colorScheme="blue" textColor="white" onClick={onCheckoutBtn}>
+                Checkout
+              </Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
+  )
 }
