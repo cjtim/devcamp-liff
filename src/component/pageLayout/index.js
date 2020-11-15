270a13117a413846409ff453c34bc7152585dd7f
Tim <iam@cjtim.com>
2020-11-15 15:35:23 +0700

    add cart on buttom css

--- src/component/pageLayout/index.js
+++ src/component/pageLayout/index.js
@@ -1,17 +1,21 @@
+import { Box, Flex } from '@chakra-ui/react'
 import React from 'react'
 import { useRecoilValue } from 'recoil'
-import {cart as atomCart} from '../../recoil'
+import { cart as atomCart } from '../../recoil'
 import CartDrawer from '../cartDrawer'
 function PageLayout({ children }) {
-    const cart = useRecoilValue(atomCart)
-    React.useState(() => {
-        
-    }, [])
-    return (
-        <main>
-            {children}
-            {cart.selectedMenu && <CartDrawer/>}
-        </main>
-    )
+  const cart = useRecoilValue(atomCart)
+  React.useState(() => {}, [])
+  return (
+    <Flex minH="100vh" flexDirection="column">
+      <main style={{flex: 1}}>
+        {children}
+      </main>
+      <footer>
+        <Box>{cart.selectedMenu && <CartDrawer />}</Box>
+      </footer>
+    </Flex>
+  )
 }
+
 export { PageLayout }
