29eab060fcaf8958d6e45607d77637396c924e49
Tim <iam@cjtim.com>
2020-11-30 11:47:05 +0700

    update fetching order list

--- src/function/api.js
+++ src/function/api.js
@@ -10,7 +10,10 @@ const backendInstance = axios.create({
 export function useAPI(url, payload = {}) {
   backendInstance.defaults.headers['authorization'] = `Bearer ${useRecoilValue(lineAcctoken)}`
   const api = url => backendInstance.post(url, payload).then(res => res.data)
-  const { data, error } = useSWR(url, api)
+  const { data, error } = useSWR(url, api, {
+    errorRetryInterval: 300,
+    errorRetryCount: 10
+  })
   const newErr = error && !error.status === 403
   return { data: data, error: newErr, isLoading: !error && !data }
 }
--- src/page/order/index.js
+++ src/page/order/index.js
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Route, Switch, useRouteMatch } from 'react-router-dom'
-import axios from 'axios'
-import liff from '@line/liff'
 import { LoadingAnimation } from '../../component/loadingAnimation'
 import { OrderCard } from '../../component/orderCard'
 import { Container, Text, Tabs, Tab, TabList, TabPanels, TabPanel } from '@chakra-ui/react'
 import { DetailHome } from './detail'
+import { useAPI } from '../../function/api'
 
-const backendInstance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL
-})
 export function OrderRoute() {
   const match = useRouteMatch()
   return (
@@ -22,57 +18,50 @@ export function OrderRoute() {
 }
 
 function OrderHome() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [orderPayload, setOrderPayload] = useState([])
-  useEffect(() => {
-    ; (async () => {
-      try {
-        await liff.ready
-        backendInstance.defaults.headers['authorization'] = `Bearer ${liff.getAccessToken()}`
-        const api = await backendInstance.post('/order/list')
-        setOrderPayload(api.data)
-        console.log(api.data)
+  const { data: orderPayload, isLoading } = useAPI('/order/list')
 
-        setIsLoading(false)
-      } catch (e) {
-        setIsLoading(false)
-        alert(e.message)
-      }
-    })()
-  }, [])
   if (!isLoading)
     return (
       <>
-        <Container py={2} >
-          <Text fontSize="3xl" marginTop="10px">Order Detail</Text>
-            <Tabs variant="unsytle" marginTop="20px" marginBottom="20px">
-              <TabList>
-                <Tab _selected={{ color: "white", bg: "red.300" }} borderRadius="20px">COOKING</Tab>
-                <Tab _selected={{ color: "white", bg: "green.300" }} borderRadius="20px">COMPLETE</Tab>
-              </TabList>
-              <TabPanels>
-                <TabPanel>
-                  {orderPayload &&
-                    orderPayload.filter((i) => i.status === "COOKING").map((i, index) => {
+        <Container py={2}>
+          <Text fontSize="3xl" marginTop="10px">
+            Order Detail
+          </Text>
+          <Tabs variant="unsytle" marginTop="20px" marginBottom="20px">
+            <TabList>
+              <Tab _selected={{ color: 'white', bg: 'red.300' }} borderRadius="20px">
+                COOKING
+              </Tab>
+              <Tab _selected={{ color: 'white', bg: 'green.300' }} borderRadius="20px">
+                COMPLETE
+              </Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                {orderPayload &&
+                  orderPayload
+                    .filter(i => i.status === 'COOKING')
+                    .map((i, index) => {
                       if (index < 10) {
-                        return <OrderCard key={index} order={i} statusColor="#F56565"/>
+                        return <OrderCard key={index} order={i} statusColor="#F56565" />
                       }
-                      return ""
+                      return ''
                     })}
-                </TabPanel>
-                <TabPanel>
-                  {orderPayload &&
-                      orderPayload.filter((i) => i.status === "COMPLETE").map((i, index) => {
-                        if (index < 10) {
-                          return <OrderCard key={index} order={i} statusColor="#68D391"/>
-                        }
-                        return ""
-                      })}
-                </TabPanel>
-              </TabPanels>
+              </TabPanel>
+              <TabPanel>
+                {orderPayload &&
+                  orderPayload
+                    .filter(i => i.status === 'COMPLETE')
+                    .map((i, index) => {
+                      if (index < 10) {
+                        return <OrderCard key={index} order={i} statusColor="#68D391" />
+                      }
+                      return ''
+                    })}
+              </TabPanel>
+            </TabPanels>
 
-
-          {/* {
+            {/* {
             orderPayload &&
             orderPayload.map((i, index) => {
               if (index < 10) {
