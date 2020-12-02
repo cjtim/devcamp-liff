f96a46e4d2c1c28286c95ce53881ebbeadd14c4b
pirapat jitcharoenwirakun <pirapat.j@ku.th>
2020-12-03 01:55:15 +0700

    add search button in restaurant all menu

--- src/page/restaurant/allMenu.js
+++ src/page/restaurant/allMenu.js
@@ -4,7 +4,8 @@ import { useParams } from 'react-router-dom'
 import { MenuCard } from '../../component/menuCard'
 import { useAPI } from '../../function/api'
 import { CartDrawer } from '../../component/cartDrawer'
-import { Box, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, IconButton, Text, VStack } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
 
 export function RestaurantMenu() {
   let { restaurantId } = useParams()
@@ -12,7 +13,7 @@ export function RestaurantMenu() {
     restaurantId: restaurantId
   })
 
-  const { data: resName, isLoading: isWaitRestaurant} = useAPI('/restaurant/get', {
+  const { data: resName, isLoading: isWaitRestaurant } = useAPI('/restaurant/get', {
     restaurantId: restaurantId
   })
 
@@ -20,9 +21,12 @@ export function RestaurantMenu() {
   return (
     <>
       <Box ml="20px" mt="20px" mb="20px">
-        <Text fontSize="2xl" fontWeight="bold">{resName && resName.name}</Text>
+        <Text fontSize="2xl" fontWeight="bold">
+          {resName && resName.name}
+        </Text>
+        <IconButton as="a" href={"/restaurant/search/" + restaurantId} aria-label="Search database" marginLeft="auto" icon={<SearchIcon />} />
       </Box>
-      <VStack align="stretch" maxW={window.innerWidth} spacing ={4}>
+      <VStack align="stretch" maxW={window.innerWidth} spacing={4}>
         {data &&
           data.map((menu, index) => {
             return <MenuCard key={index} menu={menu} />
